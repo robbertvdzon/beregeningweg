329f5868cb7606055ad2b4e2285790a598e1414b
Robbert vd Zon <robbert.vanderzon@edsn.nl>
2024-11-29 16:50:11 +0100

    sproeier

--- lib/main.dart
+++ lib/main.dart
@@ -103,6 +103,17 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  // void _logout() async{
+  //   setState(() {
+  //     try {
+  //       await FirebaseAuth.instance.signOut();
+  //       print("Gebruiker is uitgelogd.");
+  //     } catch (e) {
+  //       print("Error tijdens uitloggen: $e");
+  //     }
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -125,19 +136,6 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ElevatedButton(
@@ -147,6 +145,16 @@ class _MyHomePageState extends State<MyHomePage> {
             ElevatedButton(
                 onPressed: _incrementCounter,
                 child: Text('+2')
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  await FirebaseAuth.instance.signOut();
+                  // Ga terug naar de loginpagina
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+                },
+                child: Text('logout')
             ),
           ],
         ),
